305268a85dfd97bf181918edcb956cef0c8e2dd2
chengsiyuan <chengsiyuan@acoinfo.com>
2025-02-08 10:20:08 +0800

    Try fix getrandom test on sylixos
    Fix getrandom test on sylixos

---
+++ src/internal/syscall/unix/asm_sylixos.s
@@ -0,0 +1,7 @@
+// Copyright 2025 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+#include "textflag.h"
+
+TEXT ·libc_getrandom_trampoline(SB),NOSPLIT,$0-0; JMP libc_getrandom(SB)
